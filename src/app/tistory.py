5c944af3f923e44696214b8fa908479b498dcfd9
Youngwoo Lee <mvl100d@gmail.com>
2023-05-16 16:12:51 +0900

    티스토리 이미지 업로드 및 api 추가

--- src/app/tistory.py
+++ src/app/tistory.py
@@ -1,14 +1,22 @@
 from app.config import *
 import requests
 import subprocess
+import markdown
 
-def post_to_tistory(access_token, blog_name, title, content):
+def post_to_tistory(title, path):
+
+    with open(path, 'r') as f:
+        content = f.read()
+
+    content = markdown.markdown(content)
+
     url = f"https://www.tistory.com/apis/post/write"
     
     data = {
-        'access_token': access_token,
+        'access_token': TISTORY_ACCESS_TOKEN,
         'output': 'json',
-        'blogName': blog_name,
+        'blogName': BLOG_NAME,
+        'visibility':3,
         'title': title,
         'content': content
     }
@@ -26,13 +34,39 @@ def post_to_tistory(access_token, blog_name, title, content):
     else:
         root_logger.critical("API 응답 형식이 잘못되었습니다.")
 
+
+def upload_to_tistory(path):
+    requests_headers = {
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36"
+    }
+
+    files = {'uploadedfile': open(path, 'rb')}
+
+    url = f'https://www.tistory.com/apis/post/attach?access_token={TISTORY_ACCESS_TOKEN}&blogName={BLOG_NAME}&output=json'
+
+    response = requests.post(url, files=files,headers=requests_headers)
+    result = response.json()
+
+    if 'tistory' in result and 'status' in result['tistory']:
+        status = result['tistory']['status']
+        if status == '200':
+            root_logger.critical("이미지가 성공적으로 업로드되었습니다.")
+            return result['tistory']['replacer']
+        else:
+            root_logger.critical(f"이미지 업로드에 실패하였습니다. 오류 코드: {status}")
+            root_logger.critical(result)
+            return ""
+    else:
+        root_logger.critical("API 응답 형식이 잘못되었습니다.")
+        return ""
+
 '''
 path : markdown 파일의 경로
 title : 포스팅 제목
 content : 포스팅 내용
 https://github.com/jojoldu/markdown-tistory
 '''
-def post_to_tistory_markdown(path, title, content):
+def post_to_tistory_markdown(title, path):
     out = subprocess.run(['/usr/local/bin/markdown-tistory', 'write' , path], capture_output=True)
     root_logger.critical(f"{out.stdout}")
 
@@ -41,3 +75,9 @@ markdown 맨 뒤에 이미지를 추가하는 함수
 '''
 def insert_image_into_markdown(md_content, img_path):
     return f"{md_content} \n\n\n ![image]({img_path})"
+
+'''
+tistory image 추가
+'''
+def insert_tistory_image_into_markdown(md_content, replacer):
+    return f"{md_content} \n\n\n <p>{replacer}</p>"
--- src/main.py
+++ src/main.py
@@ -2,22 +2,30 @@ from app import *
     
 from flask import Flask, render_template, request
 
-def post_process(input_value) :
+def conscience(content):
+    return f"{content} \n\n\n\n 이 글은 ChatGPT API를 이용하여 자동으로 작성된 글입니다."
 
+def post_process(input_value) :
+    root_logger.critical(f"{input_value}에 대한 글 작성 시작...")
     post_subject = input_value
     prompt = f"'{post_subject}'에 대한 주제의 블로그 글, 자세하게, markdown"
     response = generate_text(prompt, max_token=MAX_TOKEN)
     root_logger.critical(response)
 
     prompt = make_prompt_for_image(post_subject)
+    
     img_path = make_image_karlo(post_subject, prompt)
     #img_path = make_image_dall_e(post_subject, prompt)
-    response = insert_image_into_markdown(response, img_path)
+    
+    #response = insert_image_into_markdown(response, img_path)
+    response = insert_tistory_image_into_markdown(response, upload_to_tistory(img_path))
 
-    post_path = drop_to_markdown(post_subject, response)
+    response = conscience(response)
 
-    #post_to_tistory(TISTORY_ACCESS_TOKEN, BLOG_NAME, title, response)
-    post_to_tistory_markdown(post_path, post_subject, response)
+    post_path = drop_to_markdown(f"{post_subject}", response)
+
+    #post_to_tistory_markdown(post_subject, post_path)
+    post_to_tistory(post_subject, post_path)
 
 app = Flask(__name__)
 
@@ -33,6 +41,21 @@ def process():
         post_process(input_value)
 
         return f"'{input_value}'를 주제로 포스팅 완료!"
+    else:
+        return '잘못된 요청입니다.'
+
+@app.route('/uploadpost', methods=['GET'])
+def uploadpost():
+    if request.method == 'GET' or request.method == 'POST' :
+        input_value = request.args.get('input')
+        
+        if input_value is not None :
+            post_process(input_value)
+            return f"'{input_value}'를 주제로 포스팅 완료!"
+
+        else :
+            return f"wrong input value"
+            
     else:
         return '잘못된 요청입니다.'
 
