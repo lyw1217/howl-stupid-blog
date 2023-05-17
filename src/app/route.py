620205ad14e5125a1fffec7bbacd0857a29915f4
Youngwoo Lee <mvl100d@gmail.com>
2023-05-17 14:12:13 +0900

    영어 포스팅 추가 및 목록조회 추가

--- src/app/route.py
+++ src/app/route.py
@@ -1,58 +1,45 @@
-from flask import Flask, render_template, request
+#-*- coding:utf-8 -*-
+from flask import Flask, render_template, request, jsonify
 from app import *
 
-import threading
-import time
-import random
+flask_app = Flask(__name__)
 
-app = Flask(__name__)
-
-@app.route('/')
+if SYS_PLATFORM == 'Windows':
+    SUBJECT_PATH = os.path.join(ROOT_DIR, 'config\subject.json')
+else:
+    SUBJECT_PATH = os.path.join(ROOT_DIR, 'config/subject.json')
+    
+@flask_app.route('/')
 def index():
     return render_template('index.html')
 
-'''
-@app.route('/process', methods=['GET', 'POST'])
-def process():
-    if request.method == 'POST':
-        input_value = request.form['myInput']
-
-        post_process(input_value)
-
-        return f"'{input_value}'를 주제로 포스팅 완료!"
-    else:
-        return '잘못된 요청입니다.'
-'''
-@app.route('/process', methods=['GET', 'POST'])
+@flask_app.route('/process', methods=['GET', 'POST'])
 def add():
     if request.method == 'POST':
-
-        if SYS_PLATFORM == 'Windows':
-            SUBJECT_PATH = os.path.join(ROOT_DIR, 'config\subject.json')
-        else:
-            SUBJECT_PATH = os.path.join(ROOT_DIR, 'config/subject.json')
         if os.path.isfile(SUBJECT_PATH):
             with open(SUBJECT_PATH) as json_file:
                 subjects = json.load(json_file)
         else :
             root_logger.critical("subject.json 파일 읽기 실패")
             return f"주제 : '{input_value}', 포스팅 대기열 추가 실패.. 다시 시도해주세요."
-
+        
         input_value = request.form['myInput']
         if len(input_value) > 0:
-            subjects['subject'].append(input_value)
+            subjects['subjects'].append(input_value)
+        else :
+            return f"주제가 잘못되었습니다. 다시 시도해주세요."
 
         with open(SUBJECT_PATH, 'w') as json_file:
-            json.dump(subjects, json_file)
+            json.dump(subjects, json_file, ensure_ascii=False)
 
-        return f"주제 : '{input_value}', 포스팅 대기열 추가 완료! ({len(subjects['subject'])}개 남음)"
+        return f"주제 : '{input_value}', 포스팅 대기열 추가 완료! ({len(subjects['subjects'])}개 남음)"
     else:
         return '잘못된 요청입니다.'
 
 
-@app.route('/uploadpost', methods=['GET', 'POST'])
+@flask_app.route('/uploadpost', methods=['GET', 'POST'])
 def uploadpost():
-    if request.method == 'GET' or request.method == 'POST' :
+    if request.method == 'POST' :
         input_value = request.args.get('input')
         
         if input_value is not None :
@@ -61,65 +48,15 @@ def uploadpost():
 
         else :
             return f"wrong input value"
-            
+    elif request.method == 'GET' :
+        if os.path.isfile(SUBJECT_PATH):
+            with open(SUBJECT_PATH) as json_file:
+                subjects = json.load(json_file)
+        else :
+            root_logger.critical("subject.json 파일 읽기 실패")
+            return f"포스팅 대기열 조회 실패.. 다시 시도해주세요."
+        
+        return jsonify(subjects)
     else:
         return '잘못된 요청입니다.'
 
-class Worker(threading.Thread):
-    def __init__(self):
-        super().__init__()
-
-    def run(self):
-        root_logger.critical(f"subject upload daemon 시작, interval = {INTERVAL}")
-        time.sleep(5)
-        while True:
-            if SYS_PLATFORM == 'Windows':
-                SUBJECT_PATH = os.path.join(ROOT_DIR, 'config\subject.json')
-            else:
-                SUBJECT_PATH = os.path.join(ROOT_DIR, 'config/subject.json')
-            if os.path.isfile(SUBJECT_PATH):
-                with open(SUBJECT_PATH) as json_file:
-                    subjects = json.load(json_file)
-            else :
-                root_logger.critical("subject.json 파일 읽기 실패")
-                return None
-            
-            if len(subjects['subject']) > 0 :
-                s = subjects['subject'][0]
-                
-                del subjects['subject'][0]
-                with open(SUBJECT_PATH, 'w') as json_file:
-                    json.dump(subjects, json_file)
-
-                if len(s) > 0:
-                    post_process(s)
-                
-            root_logger.critical(f"남은 subject 개수 : {len(subjects['subject'])}")
-
-            # 주기가 너무 일정하지 않게끔
-            time.sleep(INTERVAL * random.randint(40, 60) + random.randint(0, 100))
-
-def conscience(content):
-    return f"{content} \n\n\n\n 이 글은 ChatGPT API를 이용하여 자동으로 작성된 글입니다."
-
-def post_process(input_value) :
-    root_logger.critical(f"{input_value}에 대한 글 작성 시작...")
-    post_subject = input_value
-    prompt = f"'{post_subject}'에 대한 주제의 블로그 글, 자세하게, markdown"
-    response = generate_text(prompt, max_token=MAX_TOKEN)
-    root_logger.critical(response)
-
-    prompt = make_prompt_for_image(post_subject)
-    
-    img_path = make_image_karlo(post_subject, prompt)
-    #img_path = make_image_dall_e(post_subject, prompt)
-    
-    #response = insert_image_into_markdown(response, img_path)
-    response = insert_tistory_image_into_markdown(response, upload_to_tistory(img_path))
-
-    response = conscience(response)
-
-    post_path = drop_to_markdown(f"{post_subject}", response)
-
-    #post_to_tistory_markdown(post_subject, post_path)
-    post_to_tistory(post_subject, post_path)
--- src/main.py
+++ src/main.py
@@ -1,5 +1,10 @@
 from app import *
 
+from threading import Thread
+import time
+import random
+import datetime
+
 def print_initlog():
     root_logger.critical(r"██   ██  ██████  ██     ██ ██                ")
     root_logger.critical(r"██   ██ ██    ██ ██     ██ ██                ")
@@ -30,13 +35,103 @@ def print_initlog():
     root_logger.critical(f"  INTERVAL     = {INTERVAL} min")
     root_logger.critical(r"======================================")
 
+'''
+쓰레드
+'''
+def th_subject_upload():
+    root_logger.critical(f"subject upload thread 시작, interval = {INTERVAL}")
+    while True:
+        if SYS_PLATFORM == 'Windows':
+            SUBJECT_PATH = os.path.join(ROOT_DIR, 'config\subject.json')
+        else:
+            SUBJECT_PATH = os.path.join(ROOT_DIR, 'config/subject.json')
+        if os.path.isfile(SUBJECT_PATH):
+            with open(SUBJECT_PATH) as json_file:
+                subjects = json.load(json_file)
+        else :
+            root_logger.critical("subject.json 파일 읽기 실패")
+            return None
+        
+        if len(subjects['subjects']) > 0 :
+            s = subjects['subjects'][0]
+            
+            del subjects['subjects'][0]
+            with open(SUBJECT_PATH, 'w') as json_file:
+                json.dump(subjects, json_file, ensure_ascii=False)
+
+            if len(s) > 0:
+                post_process(s)
+        
+        next_interval = INTERVAL * random.randint(40, 60)
+
+        root_logger.critical(f"남은 subject 개수    : {len(subjects['subjects'])}")
+        root_logger.critical(f"다음 포스팅 예상 시간 : {datetime.datetime.now() + datetime.timedelta(seconds=next_interval)}")
+
+        # 주기가 너무 일정하지 않게끔
+        time.sleep(next_interval)
+
+'''
+일말의 양심
+'''
+def conscience(content, flag):
+    if flag :
+        return f"{content} \n\n\n\n 이 글은 ChatGPT, DALL-E, Karlo API를 이용하여 자동으로 작성된 글입니다."
+    else :
+        return f"{content} \n\n\n\n This post was automatically generated using the ChatGPT, DALL-E, and Karlo APIs."
+
+
+'''
+포스트 업로드를 위한 메인 프로세스
+'''
+def post_process(input_value) :
+    root_logger.critical(f"{input_value}에 대한 글 작성 시작...")
+    
+    post_subject = input_value
+    
+    # 주제에 맞는 컨텐츠 생성
+    prompt = f"'{post_subject}'에 대한 주제의 블로그 글, 자세하게, markdown"
+    response = generate_text(prompt, max_token=MAX_TOKEN)
+    root_logger.critical(response)
+
+    # 영어로 번역
+    en_contents = translate_to_en(response)
+
+    # 이미지를 위한 프롬프트 생성
+    prompt = make_prompt_for_image(post_subject)
+    
+    # 생성한 프롬프트로 이미지 생성
+    img_path = make_image_karlo(post_subject, prompt)
+    
+    # 티스토리 이미지 삽입을 위해 이미지 업로드, replacer 컨텐츠 하단에 추가
+    replacer = upload_to_tistory(img_path)
+    en_contents = insert_tistory_image_into_markdown(en_contents, replacer)
+    kr_contents = insert_tistory_image_into_markdown(response, replacer)
+
+    # 일말의 양심 추가
+    kr_contents = conscience(kr_contents, True)
+    en_contents = conscience(en_contents, False)
+
+    # 포스팅 제목 생성
+    kr_post_subject = f"{post_subject}"
+    en_post_subject = f"{translate_to_en(post_subject)}"
+
+    # 마크다운 파일로 떨구기
+    kr_post_path = drop_to_markdown(kr_post_subject, kr_contents)
+    en_post_path = drop_to_markdown(en_post_subject, en_contents)
+
+    # 티스토리에 업로드
+    post_to_tistory(kr_post_subject, kr_post_path)
+    
+    # 약 1~5분 뒤 영어 포스팅 업로드
+    time.sleep(random.randint(60, 300))
+    
+    post_to_tistory(en_post_subject, en_post_path)
 
 if __name__ == "__main__":
 
     print_initlog()
 
-    t = Worker()     # sub thread 생성
-    t.daemon = True
-    t.start()        # sub thread의 run 메서드를 호출
+    t = Thread(target=th_subject_upload)
+    t.start()
 
-    app.run(debug=True, port=8080)
+    flask_app.run(debug=True, port=8080)
